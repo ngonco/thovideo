7dbafd7f428a9d7d7f061bb04a8acce6bcbf2e8a
ngonco <133347650+ngonco@users.noreply.github.com>
2026-02-03 16:55:09 +0700

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -255,7 +255,7 @@ def get_app_style():
        ==================================================================== */
     
     /* 1. Áp dụng cho toàn bộ ứng dụng */
-    html, body, [class*="css"], .stApp {{
+    html, body, .stApp {{
         font-family: 'Arial', 'Helvetica', sans-serif !important;
         color: #3E2723;
     }}
